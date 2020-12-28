6bcecba9a7051df397aca343532b258bb0659a69
EEisbrenner <gitbrenner@pm.me>
2020-12-28 14:15:14 +0100

    point wise anomaly calculation (#14)
    1. Modify test data set to multi-year representation
    
    The test data now consists of three years of January data instead of one
    all data from one year.
    
    2. Refactoring and bug fixes of processing steps
    
    + Fix bug in rm_leap, days weren't calculated prolerly
    + Refactor trend removal and climatorlogy calculation
        + now both have point_wise as default

--- src/water_masses/processing.py
+++ src/water_masses/processing.py
@@ -5,9 +5,9 @@ from pathlib import Path
 from typing import Optional
 
 import intake
+import numpy as np
 import pandas as pd
 import xarray as xr
-from functools import partial
 from cftime import DatetimeNoLeap
 from statsmodels import api as sm
 
@@ -23,6 +23,7 @@ def open_sss(
             source
         ]
         .to_dask()
+        .chunk({"time": -1})
         .rename(rename_dict)
     )
 
@@ -53,15 +54,13 @@ class MetaData(object):
 
     def __init__(
         self,
-        lag: int,
         source: str,
-        clim_method: str = "domain_wide",
+        clim_method: str = "point_wise",
         averaging_method: str = "mean",
         quantile: Optional[float] = None,
         test: bool = True,
     ) -> None:
         """Init."""
-        self.lags = [30 * n for n in range(lag)]
         self.source = source
         self.averaging_method = averaging_method
         self.test = test
@@ -69,40 +68,99 @@ class MetaData(object):
         self.quantile = quantile
 
 
-def find_trend(da: xr.DataArray, meta_data):
-    """Find trend."""
-    data = da.values
-    shape = data.shape
-    result = getattr(
-        pd.DataFrame(data.reshape(shape[0], -1)).dropna(axis=1),
-        meta_data.averaging_method,
-    )(meta_data.quantile, axis=1)
-    index = result.index
+class RemoveTrend(object):
+    """Container class for trend removal functions."""
 
-    model = sm.OLS(result, sm.add_constant(index))
-    fit = model.fit()
+    def __init__(self, data, meta_data) -> None:
+        """Initialize RemoveTrend."""
+        self.data = data
+        self.averaging_method = meta_data.averaging_method
+        self.quantile = meta_data.quantile
 
-    trend = fit.params.x1 * index + fit.params.const
+    @staticmethod
+    def _domain_wide(data: np.ndarray, averaging_method: str, quantile: float):
+        """Find trend."""
+        shape = data.shape
 
-    return trend, model, fit
+        data = getattr(
+            pd.DataFrame(data.reshape(shape[0], -1)).dropna(axis=1),
+            averaging_method,
+        )(quantile, axis=1)
+        index = data.index
+
+        model = sm.OLS(data, sm.add_constant(index))
+        fit = model.fit()
+
+        return fit.params.x1 * index + fit.params.const
+
+    def domain_wide(self):
+        """Remove global trend from data."""
+        return self.data - xr.DataArray(
+            self._domain_wide(self.data.values, self.averaging_method, self.quantile),
+            [("time", self.data.time.values)],
+        )
+
+    @staticmethod
+    def _point_wise(data: xr.DataArray) -> np.ndarray:
+        """Remove trend from 1D time series."""
+        index = np.arange(len(data)) / len(data)
+        model = sm.OLS(np.asarray(data), sm.add_constant(index))
+        fit = model.fit()
+        trend = fit.params[1] * index + fit.params[0]
+
+        return data - trend
+
+    def point_wise(self):
+        """Calculate and remove temporal trends for each point in space."""
+        return xr.apply_ufunc(
+            self._point_wise,
+            self.data,
+            input_core_dims=[["time"]],
+            output_core_dims=[["time"]],
+            dask="parallelized",
+            vectorize=True,
+        )
 
 
 class Climatology(object):
     """Container for climatology calculation approaches."""
 
     @staticmethod
-    def domain_wide(dda_grouped: xr.DataArray, meta_data: MetaData) -> xr.DataArray:
+    def _climatology(da: xr.DataArray, md: MetaData, **kwargs) -> xr.DataArray:
+        if md.averaging_method == "mean":
+            da = getattr(da, md.averaging_method)(**kwargs)
+        elif md.averaging_method == "quantile":
+            da = getattr(da, md.averaging_method)(
+                md.quantile,
+                **kwargs,
+            )
+        else:
+            raise NotImplementedError(
+                "Only functions mean and quantile are implemented for "
+                "climatology calculation.",
+            )
+        return da
+
+    @classmethod
+    def domain_wide(
+        cls,
+        dda_grouped: xr.DataArray,
+        meta_data: MetaData,
+    ) -> xr.DataArray:
         """Calculate climatology for whole domain."""
-        return getattr(dda_grouped, meta_data.averaging_method)(
-            meta_data.quantile,
+        return cls._climatology(
+            dda_grouped,
+            meta_data,
             dim=["longitude", "latitude"],
         )
 
-    @staticmethod
-    def point_wise(dda_grouped: xr.DataArray, meta_data: MetaData) -> xr.DataArray:
+    @classmethod
+    def point_wise(cls, dda_grouped: xr.DataArray, meta_data: MetaData) -> xr.DataArray:
         """Calculate climatology for each horizontal point."""
-        return getattr(dda_grouped, meta_data.averaging_method)(
-            meta_data.quantile,
+        return cls._climatology(
+            dda_grouped,
+            meta_data,
+            dim="time",
         )
 
 
@@ -113,56 +171,62 @@ def rm_leap(data: xr.DataArray) -> xr.DataArray:
     data = data.sel(
         time=~((data.time.dt.month == february) & (data.time.dt.day == leap_day)),
     )
+    days_since_first = np.cumsum(
+        np.append(data.time[1:].values - data.time[:-1].values, timedelta(1)),
+    )
     data["time"] = [
-        DatetimeNoLeap(*data.time[0].values.tolist().timetuple()) + timedelta(d)
-        for d, _ in enumerate(data.time)
+        DatetimeNoLeap(*data.time[0].values.tolist().timetuple()) + dt
+        for dt in days_since_first
     ]
 
     return data
 
 
+def output_directory(test: bool = True) -> Path:
+    """Provide path to output directory."""
+    if test:
+        return Path.home().joinpath("data", "output", "water-masses", "test")
+    else:
+        return Path.home().joinpath("data", "output", "water-masses")
+
+
 def main(
     averaging_method: str = "quantile",
     quantile: float = 0.9,
-    clim_method: str = "domain_wide",
+    clim_method: str = "point_wise",
     test: bool = True,
 ) -> None:
     """Load, detrend and declimatize SSS data."""
-    if test:
-        output_path = Path.home().joinpath("data", "output", "water-masses", "test")
-        meta_data_partial = partial(MetaData, 3)
-    else:
-        output_path = Path.home().joinpath("data", "output", "water-masses")
-        meta_data_partial = partial(MetaData, 24)
+    source = "daily_mean" if not test else "test_daily_mean"
+    output_path = output_directory(test=test)
     output_path.mkdir(parents=True, exist_ok=True)
-    meta_data = meta_data_partial(
-        "daily_mean",
+    meta_data = MetaData(
+        source,
         clim_method=clim_method,
         averaging_method=averaging_method,
         test=test,
         quantile=quantile,
     )
 
-    # FIXME: xarray type annotation slug missing
     data = open_sss(source=meta_data.source)["salinity"]
     if test:
-        data = data.sel(time=slice("1996-01-01", "1997-12-31")).isel(
+        data = data.isel(
             longitude=slice(None, None, 10),
             latitude=slice(None, None, 10),
         )
     data = rm_leap(data)
-    data -= xr.DataArray(find_trend(data, meta_data)[0], [("time", data.time.values)])
+    data = getattr(RemoveTrend(data, meta_data), meta_data.clim_method)()
+    data = data.groupby("time.dayofyear")
     data -= getattr(Climatology, meta_data.clim_method)(
-        data.groupby("time.dayofyear"),
+        data,
         meta_data,
     )
 
     refser = ref_series(data, meta_data.averaging_method, quantile=meta_data.quantile)
 
-    data = data.to_dataset(name="SSS")  # type: ignore
-    data.to_netcdf(output_path.joinpath("sss-processed.nc"))
+    data.to_dataset(name="SSS").to_netcdf(output_path.joinpath("sss-processed.nc"))
     refser.to_netcdf(output_path.joinpath("sss_time_series_processed.nc"))
 
 
 if __name__ == "__main__":
-    main(test=True)
+    main()
