8c486e77ff861ae4fd8b7e12c3d5a5b19e17d767
Ezra Eisbrenner <gitbrenner@pm.me>
2021-11-24 17:48:48 +0100

    Update __init__.py and bump version

--- src/water_masses/__init__.py
+++ src/water_masses/__init__.py
@@ -7,21 +7,27 @@ from . import (
     processing,
     filter_month,
     time_series,
+    filtering,
+    transform,
     # submodules
     tracmass,
     origin,
 )
+from .version import pkg_version as __version__
 from .tracmass import seeding
 from .origin import pca
 
 __all__ = [
     "version",
+    "__version__",
     # modules
     "constants",
     "processing",
     "filter_month",
     "spgsi",
     "time_series",
+    "filtering",
+    "transform",
     # submodule
     "tracmass",
     "seeding",
